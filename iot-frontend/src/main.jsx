e7155db63ac84adb1724d0c35ea75872b236a8eb
Tani <danisaloriutta@hotmail.com>
2025-07-12 18:14:30 +0300

    Add frontend Auth fixes

--- iot-frontend/src/main.jsx
+++ iot-frontend/src/main.jsx
@@ -3,18 +3,25 @@ import ReactDOM from "react-dom/client"
 import { PublicClientApplication } from "@azure/msal-browser"
 import { MsalProvider } from "@azure/msal-react"
 import { msalConfig } from "./auth/authConfig"
-
+import Dashboard from "./views/Dashboard"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './custom.css'
 
 const msalInstance = new PublicClientApplication(msalConfig)
 
-msalInstance.handleRedirectPromise().then(() => {
-  ReactDOM.createRoot(document.getElementById("root")).render(
-    <React.StrictMode>
-      <MsalProvider instance={msalInstance}>
-        <App />
-      </MsalProvider>
-    </React.StrictMode>
-  )
-}).catch(error => {
-  console.error("MSAL redirect error:", error)
-})
+
+msalInstance.initialize()
+  .then(() => msalInstance.handleRedirectPromise())
+  .then(() => {
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <MsalProvider instance={msalInstance}>
+          <Dashboard />
+        </MsalProvider>
+      </React.StrictMode>
+    )
+  })
+  .catch((err) => {
+    console.error("MSAL startup error:", err)
+    document.getElementById("root").textContent = "Authentication failed. Check console."
+  })
