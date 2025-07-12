7696ab614de2e4d84bdd4a953c8204943ee0ec5b
Tani <danisaloriutta@hotmail.com>
2025-07-12 20:28:20 +0300

    Add frontend Auth fixes

--- iot-frontend/src/main.jsx
+++ iot-frontend/src/main.jsx
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom/client"
 import { PublicClientApplication } from "@azure/msal-browser"
 import { MsalProvider } from "@azure/msal-react"
 import { msalConfig } from "./auth/authConfig"
-
+import Dashboard from "./views/Dashboard"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './custom.css'
 
 const msalInstance = new PublicClientApplication(msalConfig)
 
+<<<<<<< HEAD
 async function bootstrap() {
   try {
     await msalInstance.initialize() 
@@ -31,3 +34,20 @@ async function bootstrap() {
 }
 
 bootstrap()
+=======
+
+    ReactDOM.createRoot(document.getElementById("root")).render(
+      <React.StrictMode>
+        <MsalProvider instance={msalInstance}>
+          <main className="bg-dark text-light">
+            <Dashboard />
+          </main>
+        </MsalProvider>
+      </React.StrictMode>
+    )
+  })
+  .catch((err) => {
+    console.error("MSAL startup error:", err)
+    document.getElementById("root").textContent = "Authentication failed. Check console."
+  })
+>>>>>>> e7155db (Add frontend Auth fixes)
