e1f7df0f2941eadeff5435a403ff25d17fc3c247
Tani <danisaloriutta@hotmail.com>
2025-07-12 18:05:01 +0300

    Add frontend Auth fixes

--- iot-frontend/src/main.jsx
+++ iot-frontend/src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client"
 import { PublicClientApplication } from "@azure/msal-browser"
 import { MsalProvider } from "@azure/msal-react"
 import { msalConfig } from "./auth/authConfig"
-import Dashboard from "./views/Dashboard"
-import 'bootstrap/dist/css/bootstrap.min.css'
-import './custom.css'
+
 
 const msalInstance = new PublicClientApplication(msalConfig)
 
@@ -13,12 +11,10 @@ msalInstance.handleRedirectPromise().then(() => {
   ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
       <MsalProvider instance={msalInstance}>
-        <body className="bg-dark text-light">
-          <Dashboard />
-        </body>
+        <App />
       </MsalProvider>
     </React.StrictMode>
   )
-}).catch((error) => {
-  console.error("Error handling redirect:", error)
+}).catch(error => {
+  console.error("MSAL redirect error:", error)
 })
